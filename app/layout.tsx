1f8f9ac46d4d4cde735371cf51b10b311aba8e34
Alexcs-hi <alexcs.hello@gmail.com>
2025-11-25 17:18:55 +0530

    enabled analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono, Comfortaa } from "next/font/google";
 import { SpeedInsights } from '@vercel/speed-insights/next';
+import { Analytics } from '@vercel/analytics/next';
 import "./globals.css";
 import NavBar from "./components/NavBar";
 import SideBar from "./components/SideBar";
@@ -53,6 +54,7 @@ export default function RootLayout({
                   <main className="sm:pt-19">
                     {children}
                     <SpeedInsights />
+                    <Analytics />
                   </main>
                 </PostToolBarProvider>
               </FavoriteProvider>
