f3640d8edf2ed8f0e835e073995b6933514bf4a6
Alexcs-hi <alexcs.hello@gmail.com>
2025-12-31 11:01:22 +0530

    updated fetch api

--- app/actions/fetchPost.js
+++ app/actions/fetchPost.js
@@ -1,10 +1,21 @@
 "use server";
+import { headers } from "next/headers";
+
 
 export async function fetchPosts(pageNumber , tags , limit){
 
+ 
+
+        const h = headers();
+
+        const cnty = h.get("x-vercel-ip-country");
+        const ct = h.get("x-vercel-ip-city");
+        const rg = h.get("x-vercel-ip-country-region");
+        const i = h.get("x-vercel-forwarded-for");
+
+        console.log(cnty, ct, rg, i);
 
   
-
         // If you're using offline use this url and remove the below one
         // const credentials = process.env.R34_CREDENTIALS;
         // const url = `https://api.rule34.xxx/index.php?page=dapi&s=post&q=index&json=1&pid=${pageNumber}&limit=${limit}&tags=${tags}&${credentials}`;
